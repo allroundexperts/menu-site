77aeb65c8db6adabcd0c4d15d17e3f1841033848
Sibtain Ali <allroundexperts@gmail.com>
2019-06-20 21:28:03 +0500

    Updates

---
+++ precache-manifest.36c60c010632cc09a5aa359ca651c4f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b69bdc74b57fe7ba887633491c1c4b",
+    "url": "/menu-site/index.html"
+  },
+  {
+    "revision": "8333be84cd6ab1de9500",
+    "url": "/menu-site/static/css/main.77ff6ea3.chunk.css"
+  },
+  {
+    "revision": "52a76ce5f033c3b5ef5a",
+    "url": "/menu-site/static/js/2.f21236ae.chunk.js"
+  },
+  {
+    "revision": "8333be84cd6ab1de9500",
+    "url": "/menu-site/static/js/main.f8bc029d.chunk.js"
+  },
+  {
+    "revision": "00522acdcfecdd299e73",
+    "url": "/menu-site/static/js/runtime~main.c83b3e25.js"
+  }
+]);
